21eb424e0f3324b2eaf7b320801769f494b1a1f1
Dean Willis <deanwillis@outlook.com>
2026-06-07 23:59:33 -0400

    Add 'Close' column to AnomalyTable component

--- frontend/src/components/AnomalyTable.jsx
+++ frontend/src/components/AnomalyTable.jsx
@@ -40,6 +40,7 @@ export default function AnomalyTable({ anomalies, filter, search, sort }) {
           <tr style={{ background: '#f8fafc' }}>
             <th style={th}>Ticker</th>
             <th style={th}>Date</th>
+            <th style={th}>Close</th>
             <th style={th}>Daily Return</th>
             <th style={th}>Return Z</th>
             <th style={th}>Volume Z</th>
